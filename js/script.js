d84705926958bfcf54d614415a6064971e994c8c
Mohamud Olow <khalif18@live.com>
2019-09-03 23:55:17 -0700

    enable scroll animations

---
+++ js/script.js
@@ -0,0 +1,60 @@
+window.sr = ScrollReveal();
+sr.reveal('h1', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal('.title', {
+    duration: 2000,
+    origin: 'top',
+    distance: '200px'
+});
+sr.reveal('.info', {
+    duration: 2000,
+    delay: 2000,
+    origin: 'bottom'
+});
+sr.reveal('.navbar ul', {
+    duration: 2000,
+    origin: 'bottom'
+});
+sr.reveal('#about img', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('#about .text', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('#projects .project-img', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px'
+});
+sr.reveal('#projects .text', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+sr.reveal('#contact p', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+sr.reveal('#contact button', {
+    duration: 2000,
+    delay: 2000,
+    origin: 'bottom'
+});
+sr.reveal('h2', {
+    duration: 2000,
+    origin: 'top',
+    distance: '100px'
+});
+sr.reveal('hr', {
+    duration: 2000,
+    delay: 2000,
+    origin: 'bottom'
+});
